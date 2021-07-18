64f8d195cad7441f3b9f5add659163de618bcb3a
ming851206 <aixian830@gmail.com>
2021-07-18 17:35:29 +0800

    header、footer

---
+++ js/header_footer.js
@@ -0,0 +1,83 @@
+const logo = Vue.component("the-logo", {
+    template: `
+    <div class="nav_logo">
+        <router-link to="/index">
+            <img class="logo" src="../images/logo/logo.svg" alt="logo">
+        </router-link>
+        <router-view></router-view>
+    </div>   
+    `,
+})
+const header = Vue.component("the-header", {
+    template: `
+    <header>
+        <nav class="nav">
+            <ul class="nav_list">
+                <div class="web_bar">
+                    <li><router-link to="/about">關於我們</router-link></li>
+                    <li><router-link to="/travel_list">旅遊總覽</router-link></li>
+                    <li><router-link to="/postcard">客製明信片</router-link></li>
+                    <li><router-link to="/QA">常見問題</router-link></li>
+                </div>
+                <div class="mobile_bar">
+                    <li class="home">
+                        <router-link to="/index">
+                            <img src="../images/icon/header/home_icon.svg" alt="home">
+                        </router-link>
+                    </li>
+                    <li class="favorite">
+                        <router-link to="/memberCenter">
+                            <img src="../images/icon/header/favorite_icon.svg" alt="favorite">
+                        </router-link>
+                    </li>
+                    <li class="member">
+                        <router-link to="/login">
+                            <img src="../images/icon/header/member_icon.svg" alt="member">
+                        </router-link>
+                    </li>
+                    <li class="menu">
+                        <img src="../images/icon/header/menu_icon.svg" alt="menu">
+                    </li>
+                </div>
+            </ul>
+        </nav>
+    <router-view></router-view>
+    </header>
+`,
+})
+const footer = Vue.component("the-footer", {
+    template: `
+    <footer>
+        <div class="icon_link">
+            <router-link to="/">
+                <img src="../images/icon/footer//twitter_icon.svg" alt="twitter">
+            </router-link>
+            <router-link to="/">
+                <img src="../images/icon/footer/fb_icon.svg" alt="facebook">
+            </router-link>    
+            <router-link to="/">
+                <img src="../images/icon/footer/ig_icon.svg" alt="instagram">
+            </router-link>
+            <router-view></router-view>
+        </div>
+        <div class="statement">
+            &copy; 2021 BY JUMPER
+            <br>
+            本網站為緯育TibaMe前端設計工程師班第68期學員專題作品，本平台僅供學習、展示之用。
+            <br>
+            若有侵權疑慮，您可以私訊[TibaMe-前端設計工程師養成班]，後續會由專人協助處理。
+        </div>
+    </footer>
+    `,
+})
+const router = new VueRouter({
+    mode: "history",
+})
+const vue_header = new Vue({
+    el: "#header",
+    router,
+})
+const vue_footer = new Vue({
+    el: "#footer",
+    router,
+})
