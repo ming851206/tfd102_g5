ee22737d4891d69ce246e2988548f90e2781b7e3
ming851206 <aixian830@gmail.com>
2021-07-28 11:42:42 +0800

    首頁及遊戲更新

--- js/index/index.js
+++ js/index/index.js
@@ -1,3 +1,35 @@
+// 關於我們
+const aboutUsService = Vue.component("the-service",{
+    template: `
+    <div class="about">
+        <div class="about_container">
+            <h2>我們的服務</h2>
+            <div class="all_about">
+                <div class="about_1">
+                    <img src="./images/index/content/about_1.jpg" alt="專業的線上導遊">
+                    <div class="des_1"> 
+                        <h3>專業的線上導遊</h3>
+                        <h4>我們提供具有專業知識背景的線上導遊，零時差導覽您的線上旅程，透過直播即時互動，他們將帶領您身歷其境，讓您隨時隨地，開機就能遊覽世界。</h4>
+                    </div>
+                </div>
+                <div class="about_2">
+                    <div class="des_2">
+                        <h3>提供包場</h3>
+                        <h4>在任何您舒適的環境享受這場線上社交活動，沒有陌生人，只有您和您的親朋好友，由專業的當地導遊帶領遊樂世界各處，創造獨一無二的旅遊體驗。</h4>
+                    </div>    
+                    <img src="./images/index/content/about_2.jpg" alt="提供包場">
+                </div>
+            </div>
+        </div>
+    <div class="light_bg"></div>
+    <div class="fence"></div>    
+    </div>
+    `,
+})
+const vue_service = new Vue({
+    el: "#our-service",
+})
+
 // 精選主題
 const mobileTheme = Vue.component("mobile-theme", {
     template: `
@@ -136,25 +168,33 @@ const vue_theme = new Vue({
         
 
 // 旅遊項目
-const travel = Vue.component("travel_item", {
+const travel = Vue.component("travel-item", {
     template: `
-        <ul id="travel_container">
-            <li id="travel_li" v-for="trip in trips">
-                <img class="place" :src="trip.photo" alt="看吳哥窟全景">
-                <div class="main_content">
-                    <div class="first">
-                        <div class="star_num">
-                            <img src="./images/index/content/star.svg">
-                            <p>{{trip.star}}</p>
-                        </div> 
-                        <p class="middle">({{trip.num}}).</p>
-                        <p>{{trip.place}}</p>    
+    <div class="travel">
+        <h2>熱門旅遊</h2>
+        <div id="the-travel">
+            <ul id="travel_container">
+                <li id="travel_li" v-for="trip in trips">
+                    <img class="place" :src="trip.photo" alt="看吳哥窟全景">
+                    <div class="main_content">
+                        <div class="first">
+                            <div class="star_num">
+                                <img src="./images/index/content/star.svg">
+                                <p>{{trip.star}}</p>
+                            </div> 
+                            <p class="middle">({{trip.num}}).</p>
+                            <p>{{trip.place}}</p>    
+                        </div>
+                        <h3>{{trip.title}}</h3>
+                        <p class="third">每人 $ {{trip.price}} TWD 起</p>
                     </div>
-                    <h3>{{trip.title}}</h3>
-                    <p class="third">每人 $ {{trip.price}} TWD 起</p>
-                </div>
-            </li>    
-        </ul>
+                </li>    
+            </ul>
+        </div>
+        <a href="travel_list.html">
+            <button id="index_travel_more" class="btnL_line">更多旅遊</button>
+        </a>
+    </div>
     `,
     data(){
         return {
@@ -168,53 +208,99 @@ const travel = Vue.component("travel_item", {
     }
 })
 const vue_travel = new Vue({
-    el: "#the-travel",
+    el: "#popular_travel",
+    data: {
+
+    },
+})
+
+// 選擇我們
+const video = Vue.component("the-reason",{
+    template: `
+    <div class="the_reason">
+        <div class="text">
+            <h2>為何選擇我們？</h2>
+            <ul class="reasons">
+                <li v-for="reason in reasons">{{reason}}</li>
+            </ul>
+        </div>
+        <video id="index_video" poster="./video/video_img.jpg" controlsList="nodownload" controls>
+            <source src="./video/choose_us.mp4">
+            <p>Sorry, your browser doesn't support embedded videos.</p>
+        </video>
+    </div>
+    `,
+    data(){
+        return{
+            reasons: [
+                "超過80位專業的線上導遊",
+                "超過70個位於全球各地的知名觀光景點",
+                "1200個曾舉辦過的線上旅遊",
+                "最獨一無二的線上導覽互動",
+            ]
+        }
+    }
+})
+const vue_video = new Vue({
+    el:"#choose_us",
 })
 
 
 // 更多評價
 const opinion = Vue.component("the-opinion",{
     template: `
-        <ul id="all_opinion">
-            <li class="person" v-for="person in people">
-                <div class="top">
-                    <img :src="person.avatar">
-                    <div class="right">
-                        <div class="star_num">
-                            <img src="./images/index/content//star.svg">
-                            <p>{{person.star}}</p>
-                        </div>
-                        <p>{{person.name}}</p>
-                    </div>    
-                </div>
-                <p class="content">{{person.content}}</p>
-            </li>
-        </ul>
+    <div class="more_opinion">
+        <aside class="fence_col"></aside>
+        <div class="opinion_container">
+            <h2>看看大家怎麼說 . . .</h2>
+            <div id="opinion">
+                <ul id="all_opinion">
+                <li class="person" v-for="person in people">
+                    <div class="top">
+                        <img :src="person.avatar">
+                        <div class="right">
+                            <div class="star_num">
+                                <img src="./images/index/content/star.svg">
+                                <p>{{person.star}}</p>
+                            </div>
+                            <p>{{person.name}}</p>
+                        </div>    
+                    </div>
+                    <p class="content">{{person.content}}</p>
+                </li>
+            </ul>
+            </div>
+            <button id="opinion_more" class="btnL_line">觀看更多</button>
+        </div>
+        <img id="go_top" src="./images/icon/toTop_dark.svg" alt="jump to top">
+    </div>
     `,
     data(){
         return{
             people: [
-                {avatar: "./images/index/content/people_1.svg", star: "5",  name: 'Andy', content: "這趟動物之旅對我與我的朋友來說，是一次非常棒的體驗！Diana是一位了不起的導遊，對所有動物都非常了解。很高興看到美麗的非洲大草原和所有可愛的野生動物。強烈推薦給所有人！"},
-                {avatar: "./images/index/content/people_2.svg", star: "5",  name: '王家豪', content: "這是一場非常令人享受的旅程！我非常喜歡Federica導遊，帶我們從一個艾菲場鐵塔走到凱旋門，她透過專業的直播裝備向我們展示景點細節。我們這團大約有 15 人一起參加這場活動。這是一趟生動引人入勝的巴黎之旅。Federica 是一位完美的導遊——幽默風趣、健談且知識淵博。她設法在三個多個小時內帶我們走訪了大量景點，還有講述有趣的歷史故事，我們每個人都很喜歡她，強烈推薦給所有人！"},
-                {avatar: "./images/index/content/people_3.svg", star: "5",  name: 'Bruce Lee', content: "我與家人們一起參與了一場美好的旅程。因為山本導遊是一位非常會講故事的人，他的溫暖親切透過螢幕散發出來。山本總是熱情地回答我們的問題，他帶我們認識了很多關於伏見稻荷神社和神道儀式的知識等等。歡迎任何對日本神社文化有興趣的人，來參與山本所舉行的旅遊活動！"},
-                {avatar: "./images/index/content/people_4.svg", star: "5",  name: '陳曉婷', content: "雨果真是太厲害了！我們團隊之所以報名參加這樣的旅程，是為了可以更加凝聚團隊的向心力。我們多數人都在不同的時區，使得雨果在當地時間兩點時，依舊非常有活力地帶我們體驗曼哈頓夜生活！在雨果的帶領下我們所有人都大開眼界，迫不及待地想有一天飛去紐約！推薦這種線上旅遊的方式給所有人:)"},
-                {avatar: "./images/index/content/people_5.svg", star: "5",  name: 'William Chen', content: "這是我第一次參加線上旅遊。真的很推薦Lisa導遊了。她所帶領的森林之旅非常放鬆療癒。她不僅告訴了我很多阿里山的故事，旅程沿途也教我認識了非常多植物。能跟著她的腳步線上走訪阿里山，是件非常開心的事情。"},
-                {avatar: "./images/index/content/people_6.svg", star: "5",  name: 'Silvia Lee', content: "這是我第三次參加本田導遊所舉辦的線上旅遊了。真的很喜歡本田所帶領的旅遊！這次參加他的京都旅程，彷彿我們真的從那裡穿過托里門，沿著森林的礫石步道走著。透過他的鏡頭，可以感受到溫暖的陽光穿過高大的樹木，還有潺潺的溪水聲。雖然現在因為疫情無法實際走訪京都，但參加本田的行程也是非常非常過癮啊！"},
+                {avatar: "../images/index/content/people_1.svg", star: "5",  name: 'Andy', content: "這趟動物之旅對我與我的朋友來說，是一次非常棒的體驗！Diana是一位了不起的導遊，對所有動物都非常了解。很高興看到美麗的非洲大草原和所有可愛的野生動物。強烈推薦給所有人！"},
+                {avatar: "../images/index/content/people_2.svg", star: "5",  name: '王家豪', content: "這是一場非常令人享受的旅程！我非常喜歡Federica導遊，帶我們從一個艾菲場鐵塔走到凱旋門，她透過專業的直播裝備向我們展示景點細節。我們這團大約有 15 人一起參加這場活動。這是一趟生動引人入勝的巴黎之旅。Federica 是一位完美的導遊——幽默風趣、健談且知識淵博。她設法在三個多個小時內帶我們走訪了大量景點，還有講述有趣的歷史故事，我們每個人都很喜歡她，強烈推薦給所有人！"},
+                {avatar: "../images/index/content/people_3.svg", star: "5",  name: 'Bruce Lee', content: "我與家人們一起參與了一場美好的旅程。因為山本導遊是一位非常會講故事的人，他的溫暖親切透過螢幕散發出來。山本總是熱情地回答我們的問題，他帶我們認識了很多關於伏見稻荷神社和神道儀式的知識等等。歡迎任何對日本神社文化有興趣的人，來參與山本所舉行的旅遊活動！"},
+                {avatar: "../images/index/content/people_4.svg", star: "5",  name: '陳曉婷', content: "雨果真是太厲害了！我們團隊之所以報名參加這樣的旅程，是為了可以更加凝聚團隊的向心力。我們多數人都在不同的時區，使得雨果在當地時間兩點時，依舊非常有活力地帶我們體驗曼哈頓夜生活！在雨果的帶領下我們所有人都大開眼界，迫不及待地想有一天飛去紐約！推薦這種線上旅遊的方式給所有人:)"},
+                {avatar: "../images/index/content/people_5.svg", star: "5",  name: 'William Chen', content: "這是我第一次參加線上旅遊。真的很推薦Lisa導遊了。她所帶領的森林之旅非常放鬆療癒。她不僅告訴了我很多阿里山的故事，旅程沿途也教我認識了非常多植物。能跟著她的腳步線上走訪阿里山，是件非常開心的事情。"},
+                {avatar: "../images/index/content/people_6.svg", star: "5",  name: 'Silvia Lee', content: "這是我第三次參加本田導遊所舉辦的線上旅遊了。真的很喜歡本田所帶領的旅遊！這次參加他的京都旅程，彷彿我們真的從那裡穿過托里門，沿著森林的礫石步道走著。透過他的鏡頭，可以感受到溫暖的陽光穿過高大的樹木，還有潺潺的溪水聲。雖然現在因為疫情無法實際走訪京都，但參加本田的行程也是非常非常過癮啊！"},
             ]
         }
-    }
+    },
+    mounted() {
+        $("#opinion_more").click(function(){
+            $(".person").show();
+        });
+
+        $("#go_top").click(function(){
+            $("html, body").animate({
+                scrollTop: 0
+            }, 1000)
+        })
+    },
 })
 
 const vue_opinion = new Vue({
-    el: "#opinion",
-    data: {
-        
-    }
+    el: "#more_opinion",  
 })
 
-
-$("#go_top").click(function(){
-    $("html, body").animate({
-        scrollTop: 0
-    }, 1000)
-})
