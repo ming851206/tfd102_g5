c67c0c9baeb9675dd8c479ad52ac4a34f91230d1
ming851206 <aixian830@gmail.com>
2021-08-05 13:42:33 +0800

    遊戲功能改

--- src/php/game.php
+++ src/php/game.php
@@ -1,10 +1,19 @@
 <?php
 include('./conn.php');
- 
+
+$num = json_decode(file_get_contents('php://input'), true);
+
+echo $num;
+// // 轉換陣列
+settype ($num ,"array");
+
+// 觀看陣列長怎樣
+print_r($num);
+exit();
 //建立SQL
 $sql = "SELECT task_ID, question, option1, option2, option3, answer 
         FROM JUMPER.qa_list
-        Where task_ID = 4
+        Where task_ID = 3
         order by ceiling(rand()*100) limit 3;";
 
 //執行
