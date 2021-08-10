0489ffd9cdcc46fcb4e492a2d26d7e7fe67ec9ca
Ping <pin11557@gmail.com>
2021-08-10 09:37:59 +0800

    持續調整登入頁面登入頁面php

--- src/php/Join.php
+++ src/php/Join.php
@@ -2,15 +2,15 @@
     include("./conn.php");    
 
     //建立SQL
-    $sql = "INSERT INTO member(username, password, account_status, CreateDate) VALUES (?,?,1,NOW())";
+    $sql = "INSERT INTO member(username, email, password, account_status, CreateDate) VALUES (?,?,?,1,NOW())";
 
     //執行
     $statement = getPDO()->prepare($sql);
 
     //給值
-    $statement->bindValue(1, $_POST["account"]);
-    $statement->bindValue(3, $_POST["email"]);
-    $statement->bindValue(2, $_POST["password"]);
+    $statement->bindValue(1, $_POST["account_register"]);
+    $statement->bindValue(2, $_POST["email_register"]);
+    $statement->bindValue(3, $_POST["password_register"]);
     $statement->execute();
 
     echo "<script>alert('加入成功'); location.href = '../member.html;</script>"; 
