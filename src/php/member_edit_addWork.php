7ef3f92462a4d09d50d8c1381f4725ffca4a81ed
Lee802 <zxcv566127@gmail.com>
2021-08-12 15:26:10 +0800

    會員管理旅遊頁面

---
+++ src/php/member_edit_addWork.php
@@ -0,0 +1,17 @@
+<?php
+include("./conn.php");
+
+    $getdata = json_decode(file_get_contents('php://input'), true);
+
+    //建立SQL
+    $sql = "insert into session values(null , ? , 0 , 0 , 'https://meet.google.com/fxm-isqs-umy' , ? , ?)";
+
+    //執行
+    $statement = getPDO()->prepare($sql);
+    //給值
+    $statement->bindValue(1, $getdata['index']);
+    $statement->bindValue(2, $getdata['start']);
+    $statement->bindValue(3, $getdata['end']);
+    $statement->execute();
+    echo 1;
+?>
