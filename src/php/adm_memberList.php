07c2b8be882bdf97a139806b576c060502b6e1b8
JOiCh <ilieqreila@gmail.com>
2021-08-13 17:03:38 +0800

    會員管理功能完成(開關顯示仍有bug)

---
+++ src/php/adm_memberList.php
@@ -0,0 +1,16 @@
+<?php
+
+include('./conn.php');
+
+//建立SQL 
+$sql = "SELECT ID, name, username, level, email, phone, birthday, created_at, account_status FROM JUMPER.member;";
+
+//執行
+$statement = getPDO()->prepare($sql);
+$statement->execute();
+$data = $statement->fetchAll();
+
+//回傳json
+echo json_encode($data);
+
+?>
