e1ec68c8f620ed3527435038eb28cd6381c303d7
neiloo <pixneilyang@gmail.com>
2021-08-19 15:53:30 +0800

    feat: 處理管理旅遊發送連結功能(紅點通知)

--- src/js/admin_index/child_orderlist.js
+++ src/js/admin_index/child_orderlist.js
@@ -17,13 +17,13 @@ const OrderList = {
                 <th>訂單時間</th>
                 <th>功能</th>
             </tr>
-            <tr v-for="order in filterList">
+            <tr v-for="(order, index) in filterList">
                 <td v-text="order.ID"></td>
                 <td v-text="order.name"></td>
                 <td v-text="'$'+ order.price"></td>
-                <td id="sendStatus" v-text="order.is_alerted == 1 ? '已發送':'未發送' "></td>
+                <td v-text="order.is_alerted == 1 ? '已發送':'未發送' "></td>
                 <td id="ts">{{timestampToTime(order.created_at)}}</td>
-                <td><button id="sendlinkbtn" v-if="order.is_alerted == 0" class="sendLink" @click="updateIsAlert(order.ID)">發送連結</button></td>
+                <td><button class="sendLink" @click="updateIsAlert(index)" :disabled="order.is_alerted == 1" >發送連結</button></td>
             </tr>
         </table>
         <div class="pager2">
@@ -63,8 +63,8 @@ const OrderList = {
         timestampToTime(timestamp) {
 
             var date = new Date(timestamp * 1000);//時間戳為10位需*1000，時間戳為13位的話不需乘1000
-            Y = date.getFullYear() + '/';
-            M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '/';
+            Y = date.getFullYear() + '-';
+            M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
             D = date.getDate() + ' ';
             h = date.getHours() + ':';
             m = date.getMinutes() + ':';
@@ -74,21 +74,17 @@ const OrderList = {
         },
 
         // 發送連結
-        updateIsAlert(itemId) {
-            // console.log('test');
+        updateIsAlert(index) {
+            theOrder = this.data[index];
+            // console.log(theOrder);
             axios.post('../../php/update_orderalert.php', JSON.stringify({
-                itemId: itemId,
+                itemId: theOrder.ID,
             }))
-                .then(res => {
-                    console.log(res.data);
-                    document.getElementById('sendlinkbtn').remove();    
-                    $sendStatus = document.getElementById('sendStatus');
-                    console.log($sendStatus);
-                    $sendStatus.innerText = "已發送";
-                }
-
-                )
-                .catch((error) => alert('數據加載失敗' + error));
+            .then(res => {
+                // console.log(res.data);
+                theOrder.is_alerted = 1;
+            })
+            .catch((error) => alert('數據加載失敗' + error));
         }
     },
     computed: {
