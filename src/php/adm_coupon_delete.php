ecb72736c6970fccb5a14da34c27da41e768b4c1
neiloo <pixneilyang@gmail.com>
2021-08-19 15:47:34 +0800

    update: 調整價格篩選條件

--- src/php/adm_coupon_delete.php
+++ src/php/adm_coupon_delete.php
@@ -2,8 +2,17 @@
 include('./conn.php');
 
 $ID = $_GET['theID']; // 被刪除 coupon 的 ID
-$sql = "DELETE FROM coupon WHERE ID = " . $ID;
+$now_timestamp = time(); // 抓取現在時間戳記
 
-getPDO()->exec($sql);
+// ----- 這邊是補充的語法  -----
+// $now_dateformt = date("Y-m-d H:i:s", $now_timestamp); // 將時間戳記轉換成時間格式
+// $timestamp = strtotime("23-04-2020"); // 將某特定時間轉時間戳記
 
+$sql = "UPDATE coupon SET deleted_at = ? WHERE ID = ?";
+
+
+$statement = getPDO()->prepare($sql);    
+$statement->bindValue(1 , $now_timestamp); 
+$statement->bindValue(2 , $ID); 
+$statement->execute();
 ?>
