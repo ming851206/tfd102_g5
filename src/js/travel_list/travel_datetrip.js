3f0c4970cfe3904793fdf5156c1e24f7dadd410f
neiloo <pixneilyang@gmail.com>
2021-08-21 23:52:27 +0800

    update：補上大頭貼

--- src/js/travel_list/travel_datetrip.js
+++ src/js/travel_list/travel_datetrip.js
@@ -35,7 +35,7 @@ Vue.component('filterDateTrip', {
                         <img :src="item.intro_pics">
                         <div class="content">
                             <div class="avatar">
-                                <img :src="item.intro_pics">
+                                <img :src="item.avatar">
                             </div>
                             <div class="the_icon">
                                 <div class="share" @click="share"></div>
