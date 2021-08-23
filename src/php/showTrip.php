6981715ce1d576f05e70fb1ad4fcc846e2d8ee8f
JOiCh <ilieqreila@gmail.com>
2021-08-23 23:37:34 +0800

    顯示已下架旅遊debug

--- src/php/showTrip.php
+++ src/php/showTrip.php
@@ -14,16 +14,16 @@ $cat = $_GET['cat']; // 取得分類
 //     where p.category = ? and s.started_at != ''
 //     order by s.started_at asc"; 
 
-$sql = "SELECT t2.ID, t2.place, t2.category, t2.title, t2.intro_pics, t2.link, t2.event_price, t2.avatar, round(avg(c1.star), 1) as star_num, count(product_ID) as comment_count
+$sql = "SELECT t2.ID, t2.place, t2.category, t2.title, t2.intro_pics, t2.link, t2.event_price, t2.avatar, t2.deleted_at, round(avg(c1.star), 1) as star_num, count(product_ID) as comment_count
 from comment c1
                 right join(    
-                    SELECT  p.ID, m.avatar, p.category, p.place, p.link, p.title, p.intro_pics, p.event_price, s.started_at, s.ended_at
+                    SELECT  p.ID, m.avatar, p.category, p.place, p.link, p.title, p.intro_pics, p.event_price, s.started_at, s.ended_at, p.deleted_at
                     From product_info p
                         join session s
                         on p.ID = s.product_info_ID
                         join member m
                         on p.member_ID = m.ID
-                    where p.category = ? and s.started_at != ''
+                    where p.category = ? and s.started_at != '' and p.deleted_at = ''
                     order by s.started_at asc 
                 ) t2
                 on c1.product_ID = t2.ID
